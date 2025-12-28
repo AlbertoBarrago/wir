f85028481971b8890f349a1839954649dd8415c8
Alberto Barrago <albertobarrago@gmail.com>
2025-12-28 19:26:48 +0100

    v1.0.3

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define WIR_VERSION "1.0.2"
+#define WIR_VERSION "1.0.3"
 #define WIR_NAME "wir"
 #define WIR_DESCRIPTION "What Is Running - Port and Process Inspector"
 #define WIR_AUTHOR "alBz <albertobarrago@gmail.com>"
